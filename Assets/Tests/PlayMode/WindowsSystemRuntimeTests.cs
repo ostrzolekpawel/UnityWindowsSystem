f17bd16eb7ad0e902e907b35941f8d25b6f69b14
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2024-03-10 23:19:53 +0100

    first tests with unitask

---
+++ Assets/Tests/PlayMode/WindowsSystemRuntimeTests.cs
@@ -0,0 +1,28 @@
+using Cysharp.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using OsirisGames.WindowsSystem;
+using System.Collections;
+using UnityEngine.TestTools;
+
+public class WindowsSystemRuntimeTests
+{
+    [UnityTest]
+    public IEnumerator PushWindow_ReturnUniTask() => UniTask.ToCoroutine(async () =>
+    {
+        // Arrange
+        var windowProviderMock = new Mock<IWindowProvider<int>>();
+        var windowStackMock = new Mock<IWindowStack>();
+        var windowController = new Mock<WindowControllerBase<int>>(windowProviderMock.Object, windowStackMock.Object);
+        var windowType = 1;
+        var window = new Mock<IWindow>();
+        windowStackMock.Setup(x => x.Push(window.Object)).Returns(UniTask.CompletedTask);
+        windowProviderMock.Setup(x => x.GetWindow<IWindow>(windowType)).Returns(UniTask.FromResult(window.Object));
+        // Act
+        var result = await windowController.Object.Push<IWindow>(windowType);
+
+        // Assert
+        Assert.AreEqual(window.Object, result);
+        windowStackMock.Verify(x => x.Push(window.Object), Times.Once);
+    });
+}
